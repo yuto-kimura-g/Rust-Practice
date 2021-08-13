e3e11764a25cb2fdd2462cb024bf539cebabbd9b
Yuto02D2-E2 <d.yuto.k.b@gmail.com>
2021-08-13 21:47:40 +0900

    update chap01-intro

--- tutorial/chap01-intro/cargo_hello_world/src/main.rs
+++ tutorial/chap01-intro/cargo_hello_world/src/main.rs
@@ -1,5 +1,5 @@
-use std;
 use ferris_says;
+use std;
 
 fn main() {
     let stdout = std::io::stdout();
@@ -9,19 +9,43 @@ fn main() {
     ferris_says::say(message.as_bytes(), message_len, &mut writer).unwrap();
 }
 
-
 /*
-(project name)という名前のプロジェクト(gitファイルとかcargo.tomlファイルとか色々)を作成
+MEMO:
 詳しくは，cargo new --helpを参照
+
 hoge@hige $ cargo new <project name> --bin
+(project name)という名前のプロジェクト(gitファイルとかcargo.tomlファイルとか色々)を作成
 
-(rustc?でコンパイル)
+hoge@hige $ cargo init <project name> --bin
+(project name)という名前のプロジェクトを作成
+git関係のファイルと，cargo.lockファイル，targetディレクトリは作らない
+チュートリアルではnewよりinitの方が便利かも
+
 hoge@hige $ cargo build
+(rustc?でコンパイル)
+リリース用の最適化オプション --release
 
-(コンパイルが通るかだけ確認．実際にコンパイルはしない)
 hoge@hige $ cargo check
+(コンパイルが通るかだけ確認．実際にコンパイルはしない)
 
-(コンパイルして実行まで)
 hoge@hige $ cargo run
+(コンパイルして実行まで)
+
+hoge@hige $ cargo test
+テスト？を実行してくれる
+
+hoge@hige $ cargo install clippy
+hoge@hige $ cargo clippy
+rust公式のリンターを適用
+
+hoge@hige $ rustup component add rustfmt
+hoge@hige $ cargo fmt
+現在のクレートの全ての.rsコードを自動フォーマット
+
+hoge@hige $ cargo fix
+コンパイル時のwarning(使ってない変数名をアンダーバー始まりにするとか)を自動で直してくれる
+
+hoge@hige $ cargo tree
+依存関係？を木構造で表示
 
 */
