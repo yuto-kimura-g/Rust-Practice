8b7c9f6f100948b4315da3f2a6916073e89adaa7
yuto-kimura-g <75498217+yuto-kimura-g@users.noreply.github.com>
2024-01-15 17:34:04 +0900

    update ahc028/

---
+++ procon/ahc028/README.md
@@ -0,0 +1,12 @@
+
+Rustの時の焼きなまし基本形
+
+- struct
+  - Input
+  - Output
+  - State
+- fn
+  - solve
+  - annealing
+
+こんな感じ？
--- procon/ahc028/src/main.rs
+++ procon/ahc028/src/main.rs
@@ -50,7 +50,7 @@ impl Input {
             .iter()
             .map(|row| {
                 row.iter()
-                    .map(|&c| c as usize - 'A' as usize)
+                    .map(|&c| char_to_usize(c))
                     .collect::<Vec<usize>>()
             })
             .collect::<Vec<Vec<usize>>>();
@@ -68,8 +68,9 @@ impl Input {
             .chars()
             .map(|c| char_to_usize(c))
             .collect::<Vec<usize>>();
-        let mut neighbor = vec![vec![vec![(0, 0); alphabet.len()]; N]; N];
-        let mut neighbor_dist = vec![vec![vec![u32::MAX; alphabet.len()]; N]; N];
+        let max_char_usize = *alphabet.iter().max().unwrap() + 1;
+        let mut neighbor = vec![vec![vec![(0, 0); max_char_usize]; N]; N];
+        let mut neighbor_dist = vec![vec![vec![u32::MAX; max_char_usize]; N]; N];
         for y in 0..N {
             for x in 0..N {
                 for &c in alphabet.iter() {
@@ -152,25 +153,48 @@ pub mod solver {
     struct State {
         /// ans_t[i] := t[i]を訪れる順番
         ans_t: Vec<usize>,
-        /// ans_t_head[i] := ans_t[i]の先頭の座標
-        ans_t_head: Vec<(usize, usize)>,
-        /// ans_t_tail[i] := ans_t[i]の末尾の座標
-        ans_t_tail: Vec<(usize, usize)>,
         total_cost: i64,
     }
     impl State {
         fn new() -> Self {
             State {
                 ans_t: Vec::new(),
-                ans_t_head: Vec::new(),
-                ans_t_tail: Vec::new(),
                 total_cost: 0,
             }
+        }
+
+        fn insert_mark(&mut self) {
+            self.ans_t.insert(0, usize::MAX);
+        }
+
+        fn remove_mark(&mut self) {
+            self.ans_t.remove(0);
         }
 
         /// コストからスコアを計算：O(1)
         fn eval_score(&self) -> i64 {
             max(10000 - self.total_cost, 1001)
+        }
+
+        /// ans_t からコストを再計算，スコアを計算：Θ(M)
+        fn eval_score_from_t(&mut self, input: &Input) -> i64 {
+            self.total_cost = 0;
+            let (mut cur_y, mut cur_x) = (input.sy, input.sx);
+            for &i in self.ans_t.iter() {
+                if i == usize::MAX {
+                    // skip mark
+                    continue;
+                }
+                // move cur to t[i][0]
+                self.total_cost +=
+                    input.neighbor_dist[cur_y][cur_x][*input.t[i].first().unwrap()] as i64;
+                (cur_y, cur_x) = input.neighbor[cur_y][cur_x][*input.t[i].first().unwrap()];
+                // move t[i][0] to t[i][-1]
+                let t_tail = *input.nn_path[cur_y][cur_x][i].last().unwrap();
+                self.total_cost += input.nn_path_dist[cur_y][cur_x][i] as i64;
+                (cur_y, cur_x) = t_tail;
+            }
+            self.eval_score()
         }
     }
 
@@ -183,36 +207,21 @@ pub mod solver {
         for (i, ti) in input.t.iter().enumerate() {
             // ans_t
             state.ans_t.push(i);
-            // total_cost
-            // move cur to t[i][0]
+            // total_cost: move cur to t[i][0]
             state.total_cost += input.neighbor_dist[cur_y][cur_x][ti[0]] as i64;
-            // eprintln!(
-            //     "1:cost:{}/{}, {},{},{:?}",
-            //     input.neighbor_dist[cur_y][cur_x][ti[0]],
-            //     state.total_cost,
-            //     cur_y,
-            //     cur_x,
-            //     ti.iter().map(|&c| usize_to_char(c)).collect::<Vec<char>>()
-            // );
+            // move cur to t[i][0]
             (cur_y, cur_x) = input.neighbor[cur_y][cur_x][ti[0]];
-            // ans_t_head
-            let t_head = *input.nn_path[cur_y][cur_x][i].first().unwrap();
-            state.ans_t_head.push(t_head);
-            // ans_t_tail
-            let t_tail = *input.nn_path[cur_y][cur_x][i].last().unwrap();
-            state.ans_t_tail.push(t_tail);
-            // total_cost
-            // move t[i][0] to t[i][-1]
+            // total_cost: move t[i][0] to t[i][-1]
             state.total_cost += input.nn_path_dist[cur_y][cur_x][i] as i64;
-            // eprintln!(
-            //     "2:cost:{}/{}, {},{},{}",
-            //     input.nn_path_dist[cur_y][cur_x][i], state.total_cost, cur_y, cur_x, i
-            // );
-            (cur_y, cur_x) = t_tail;
+            // move t[i][0] to t[i][-1]
+            (cur_y, cur_x) = *input.nn_path[cur_y][cur_x][i].last().unwrap();
         }
 
         // 焼く
-        // state = solver::annealing(&input, &state);
+        // TODO: overlapを考慮したい
+        state.insert_mark();
+        state = solver::annealing(&input, &state);
+        state.remove_mark();
 
         // ans_tからans_yxを作る
         let mut ans_yx = Vec::new();
@@ -226,8 +235,8 @@ pub mod solver {
 
         eprintln!(
             "Score = {}, Cost = {}",
-            state.eval_score(),
-            state.total_cost
+            state.eval_score_from_t(&input),
+            state.total_cost,
         );
         Output { ans_yx }
     }
@@ -235,12 +244,10 @@ pub mod solver {
     /// 焼く
     fn annealing(input: &Input, init_state: &State) -> State {
         let start_time = Instant::now();
-        // annealing parameter
-        // let start_temp = 10f64;
-        let start_temp = 1e-5; // 山登り
+        // parameter
+        let start_temp = 50f64;
         let end_temp = 1e-5;
         let mut temp = start_temp;
-        let delta_threshold = 3;
         let mut rng = rand_pcg::Pcg64Mcg::new(42);
         // solution
         let init_score = init_state.eval_score();
@@ -249,7 +256,6 @@ pub mod solver {
         // info
         let mut min_delta = i64::MAX;
         let mut max_delta = i64::MIN;
-        let mut mean_delta = 0;
         let mut iter_cnt: i64 = 0;
         let mut move_cnt: i64 = 0;
         let mut update_cnt: i64 = 0;
@@ -267,59 +273,30 @@ pub mod solver {
             }
 
             // 近傍操作
-            // insert(t_i, t_j): t_iをt_jの後ろに挿入
-            let i = rng.gen_range(1..(M - 4));
-            let j = rng.gen_range((i + 1)..(M - 1));
+            // shift(t_i): t_iを一番後ろに
+            // t_i-1 -> t_i -> t_i+1 ... t_e
+            // t_i-1 -> t_i+1 ... t_e -> t_i
             let mut new_state = state.clone();
+            let e = state.ans_t.len() - 1;
+            let i = rng.gen_range(1..=(e - 1));
             // ans_t
-            // t_i-1 -> t_i -> t_i+1 ... t_j-1 -> t_j -> t_j+1
-            // t_i-1 -> t_i+1 ... t_j-1 -> t_j -> t_i -> t_j+1
             new_state.ans_t.clear();
-            new_state.ans_t.extend_from_slice(&state.ans_t[..i]);
-            new_state.ans_t.extend_from_slice(&state.ans_t[(i + 1)..=j]);
-            new_state.ans_t.extend_from_slice(&state.ans_t[i..(i + 1)]);
-            new_state.ans_t.extend_from_slice(&state.ans_t[(j + 1)..]);
-            eprintln!("insert ti:{} -> tj:{}", i, j);
-            eprintln!("state|t:\n{:?}", state.ans_t);
-            eprintln!("new state|t:\n{:?}", new_state.ans_t);
-            // ans_t_head
-            // head[i+1], head[j], head[i], head[j+1]が変わる可能性あり
-            // ans_t_tail
-            // tail[i+1], tail[j], tail[i], tail[j+1]が変わる可能性あり
-            for (k1, k2) in [(i - 1, i + 1), (j - 1, j), (j, i), (i, j + 1)] {
-                let (k1_tail_y, k1_tail_x) = state.ans_t_tail[k1];
-                let k2_top_c = input.t[k2][0];
-                let (k2_head_y, k2_head_x) = input.neighbor[k1_tail_y][k1_tail_x][k2_top_c];
-                new_state.ans_t_head[k2] = (k2_head_y, k2_head_x);
-                new_state.ans_t_tail[k2] = *input.nn_path[k2_head_y][k2_head_x][k2].last().unwrap();
-            }
-
+            new_state.ans_t.extend_from_slice(&state.ans_t[..=(i - 1)]);
+            new_state.ans_t.extend_from_slice(&state.ans_t[(i + 1)..]);
+            new_state.ans_t.push(state.ans_t[i]);
             // 良くなりますか？
-            let mut cur_cost = 0;
-            cur_cost += eval_dist(state.ans_t_tail[i - 1], state.ans_t_head[i]);
-            cur_cost += eval_dist(state.ans_t_tail[i], state.ans_t_head[i + 1]);
-            cur_cost += eval_dist(state.ans_t_tail[j - 1], state.ans_t_head[j]);
-            cur_cost += eval_dist(state.ans_t_tail[j], state.ans_t_head[j + 1]);
-            let mut new_cost = 0;
-            new_cost += eval_dist(new_state.ans_t_tail[i - 1], new_state.ans_t_head[i + 1]);
-            new_cost += eval_dist(new_state.ans_t_tail[j - 1], new_state.ans_t_head[j]);
-            new_cost += eval_dist(new_state.ans_t_tail[j], new_state.ans_t_head[i]);
-            new_cost += eval_dist(new_state.ans_t_tail[i], new_state.ans_t_head[j + 1]);
-            let delta = new_cost as i64 - cur_cost as i64;
-            if delta > delta_threshold {
-                continue;
-            }
-            new_state.total_cost += delta;
+            // costは小さいほど良い
+            // scoreは大きいほど良い
+            let score_delta = new_state.eval_score_from_t(input) - state.eval_score_from_t(input);
 
             // 遷移
-            if delta <= 0 || rng.gen_bool(f64::exp(-delta as f64 / temp)) {
+            if score_delta >= 0 || rng.gen_bool(f64::exp(score_delta as f64 / temp)) {
                 // 近傍に移動してみる
                 move_cnt += 1;
                 state = new_state;
-                min_delta = min(min_delta, delta);
-                max_delta = max(max_delta, delta);
-                mean_delta += delta;
-                if state.total_cost < best_state.total_cost {
+                min_delta = min(min_delta, score_delta);
+                max_delta = max(max_delta, score_delta);
+                if state.eval_score() > best_state.eval_score() {
                     // ベストを更新した
                     update_cnt += 1;
                     best_state = state.clone();
@@ -331,16 +308,14 @@ pub mod solver {
                     );
                 }
             }
-
-            break 'main; // DEBUG:
         } // end of 'main
 
         eprintln!("==================== annealing ====================");
+        eprintln!("elapsed\t\t:{} [ms]", start_time.elapsed().as_millis());
         eprintln!("iter\tmove\t:{}", move_cnt);
         eprintln!("\tupdate\t:{}", update_cnt);
         eprintln!("\ttotal\t:{}", iter_cnt);
         eprintln!("delta\tmin/max\t:{} / {}", min_delta, max_delta);
-        eprintln!("\tmean\t:{}", mean_delta / iter_cnt);
         eprintln!("score\tinit\t:{}", init_score);
         eprintln!("\tbest\t:{}", best_state.eval_score());
         eprintln!("===================================================");
@@ -354,8 +329,8 @@ pub mod my_util {
         c as usize - 'A' as usize
     }
 
-    pub fn usize_to_char(i: usize) -> char {
-        (i as u8 + b'A') as char
+    pub fn usize_to_char(c: usize) -> char {
+        (c as u8 + b'A') as char
     }
 
     pub fn eval_dist((y, x): (usize, usize), (yy, xx): (usize, usize)) -> u32 {
